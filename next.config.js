d2fb253167dfaef592b2b625b2633a5bf27340f3
Alex Bouchard <alexbouchardd@gmail.com>
2023-05-16 10:12:24 -0400

    chore: Add next config for env

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  env: {
+    HOOKDECK_API_URL: process.env.HOOKDECK_API_URL,
+    HOOKDECK_API_KEY: process.env.HOOKDECK_API_KEY,
+  },
+  poweredByHeader: false,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
